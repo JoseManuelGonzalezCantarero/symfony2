cc9937e0fb2be1360e121e60d30edc80fe00426a
manu <sefirot_cloud@hotmail.com>
2016-02-02 10:17:33 +0100

    Lección 4 del curso Symfony2

---
+++ src/JGC/UserBundle/Controller/UserController.php
@@ -0,0 +1,11 @@
+<?php
+namespace JGC\UserBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+class UserController extends Controller
+{
+    public function indexAction()
+    {
+        return new Response('Bienvenido a mi módulo de usuarios');
+    }
+}
