07ae326b365b339b6109724471ab0a568a5e179a
manu <sefirot_cloud@hotmail.com>
2016-02-02 10:48:31 +0100

    Lección 5 del curso Symfony2

--- src/JGC/UserBundle/Controller/UserController.php
+++ src/JGC/UserBundle/Controller/UserController.php
@@ -6,6 +6,30 @@ class UserController extends Controller
 {
     public function indexAction()
     {
-        return new Response('Bienvenido a mi módulo de usuarios');
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository('JGCUserBundle:User')->findAll();
+
+        $res = 'Lista de usuarios: <br />';
+
+        foreach ($users as $user)
+        {
+            $res .= 'Usuario: '.$user->getUsername().' - Email: '.$user->getEmail().'<br />';
+        }
+
+        return new Response($res);
+    }
+
+    public function viewAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository('JGCUserBundle:User');
+
+        $user = $repository->find($id);
+
+        $res = 'Usuario con ID: '.$id.'<br />';
+
+        $res .= 'Usuario: '.$user->getUsername().' - Email: '.$user->getEmail().'<br />';
+
+        return new Response($res);
     }
 }
