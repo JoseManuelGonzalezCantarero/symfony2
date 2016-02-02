ffd4cae407eeef9e3ce5433ce01fd7ff50cc8df0
manu <sefirot_cloud@hotmail.com>
2016-02-02 16:18:57 +0100

    Lección 8 del curso Symfony2

--- src/JGC/UserBundle/Controller/UserController.php
+++ src/JGC/UserBundle/Controller/UserController.php
@@ -1,7 +1,12 @@
 <?php
 namespace JGC\UserBundle\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use JGC\UserBundle\Entity\User;
+use JGC\UserBundle\Form\UserType;
+
 class UserController extends Controller
 {
     public function indexAction()
@@ -29,10 +34,50 @@ class UserController extends Controller
 
         $user = $repository->find($id);
 
-        $res = 'Usuario con ID: '.$id.'<br />';
+        $res = 'Usuario con ID: ' . $id . '<br />';
 
-        $res .= 'Usuario: '.$user->getUsername().' - Email: '.$user->getEmail().'<br />';
+        $res .= 'Usuario: ' . $user->getUsername() . ' - Email: ' . $user->getEmail() . '<br />';
 
         return new Response($res);
+    }
+
+    public function addAction()
+    {
+        $user = new User();
+        $form = $this->createCreateForm($user);
+
+        return $this->render('JGCUserBundle:User:add.html.twig', array('form' => $form->createView()));
+    }
+
+    private function createCreateForm(User $entity)
+    {
+        $form = $this->createForm(new UserType(), $entity, array('action' => $this->generateUrl('jgc_user_create'), 'method' => 'POST'));
+        return $form;
+    }
+
+    public function createAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createCreateForm($user);
+        $form->handleRequest($request);
+
+        if ($form->isValid())
+        {
+            $password = $form->get('password')->getData();
+
+            $encoder = $this->container->get('security.password_encoder');
+            $encoded = $encoder->encodePassword($user, $password);
+
+            $user->setPassword($encoded);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('jgc_user_index');
+        }
+
+        return $this->render('JGCUserBundle:User:add.html.twig', array('form' => $form->createView()));
+
     }
 }
