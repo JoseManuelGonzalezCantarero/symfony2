5b5ddf30bd757d7b9a17272bd79e25fd91f19b5b
Manu <sefirot_cloud@hotmail.com>
2016-02-07 16:00:36 +0100

    Lección 12 del curso Symfony2

--- src/JGC/UserBundle/Controller/UserController.php
+++ src/JGC/UserBundle/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace JGC\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\FormError;
 use JGC\UserBundle\Entity\User;
 use JGC\UserBundle\Form\UserType;
 
@@ -26,16 +28,93 @@ class UserController extends Controller
         return new Response($res);
         */
 
-        $dql = "SELECT u FROM JGCUserBundle:User u";
+        $dql = "SELECT u FROM JGCUserBundle:User u ORDER BY u.id DESC";
         $users = $em->createQuery($dql);
 
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
             $users, $request->query->getInt('page', 1),
-            10
+            5
         );
 
         return $this->render('JGCUserBundle:User:index.html.twig', array('pagination' => $pagination));
+    }
+
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('JGCUserBundle:User')->find($id);
+
+        if(!$user)
+        {
+            $messageException = $this->get('translator')->trans('User not found');
+            throw $this->createNotFoundException($messageException);
+        }
+
+        $form = $this->createEditForm($user);
+
+        return $this->render('JGCUserBundle:User:edit.html.twig', array('user' => $user, 'form' => $form->createView()));
+    }
+
+    public function updateAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('JGCUserBundle:User')->find($id);
+
+        if(!$user)
+        {
+            $messageException = $this->get('translator')->trans('User not found');
+            throw $this->createNotFoundException($messageException);
+        }
+
+        $form = $this->createEditForm($user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $password = $form->get('password')->getData();
+            if(!empty(($password)))
+            {
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $password);
+                $user->setPassword($encoded);
+            }else
+            {
+                $recoverPass = $this->recoverPass($id);
+                $user->setPassword($recoverPass[0]['password']);
+            }
+
+            if($form->get('role')->getData() == 'ROLE_ADMIN')
+            {
+                $user->setIsActive(1);
+            }
+            $em->flush();
+
+            $successMessage = $this->get('translator')->trans('The user has been modified');
+            $this->addFlash('mensaje', $successMessage);
+            return $this->redirectToRoute('jgc_user_edit', array('id'=> $user->getId()));
+        }
+        return $this->render('JGCUserBundle:User:edit.html.twig', array('user'=>$user, 'form'=>$form->createView()));
+    }
+
+    private function recoverPass($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery('SELECT u.password FROM JGCUserBundle:User u WHERE u.id = :id')->setParameter('id', $id);
+
+        $currentPass = $query->getResult();
+
+        return $currentPass;
+    }
+
+    private function createEditForm(User $entity)
+    {
+        $form = $this->createForm(new UserType(), $entity, array('action'=>$this->generateUrl('jgc_user_update', array('id' => $entity->getId())), 'method'=>'PUT'));
+
+        return $form;
     }
 
     public function viewAction($id)
@@ -75,19 +154,29 @@ class UserController extends Controller
         {
             $password = $form->get('password')->getData();
 
-            $encoder = $this->container->get('security.password_encoder');
-            $encoded = $encoder->encodePassword($user, $password);
+            $passwordConstraint = new Assert\NotBlank();
+            $errorList = $this->get('validator')->validate($password, $passwordConstraint);
 
-            $user->setPassword($encoded);
+            if(count($errorList) == 0)
+            {
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $password);
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+                $user->setPassword($encoded);
 
-            $successMessage = $this->get('translator')->trans('The user has been created');
-            $this->addFlash('mensaje', $successMessage);
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
 
-            return $this->redirectToRoute('jgc_user_index');
+                $successMessage = $this->get('translator')->trans('The user has been created');
+                $this->addFlash('mensaje', $successMessage);
+
+                return $this->redirectToRoute('jgc_user_index');
+            }else
+            {
+                $errorMessage = new FormError($errorList[0]->getMessage());
+                $form->get('password')->addError($errorMessage);
+            }
         }
 
         return $this->render('JGCUserBundle:User:add.html.twig', array('form' => $form->createView()));
